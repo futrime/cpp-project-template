9caec5b2731b048b259531e2b572a6897d2c4cac
Zijian Zhang <futrime@outlook.com>
2024-03-18 18:51:24 +0800

    fix: failing to build

--- src/accumulator/Accumulator.cpp
+++ src/accumulator/Accumulator.cpp
@@ -1,5 +1,3 @@
-#pragma once
-
 #include "Accumulator.h"
 
 namespace accumulator {
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,12 @@
 #include "accumulator/Accumulator.h"
 
-#include <format>
 #include <iostream>
 
 auto main() -> int {
   auto accumulatorInstance = accumulator::Accumulator();
 
-  std::cout << std::format("Accumulator value: {}", accumulatorInstance.get());
-
   accumulatorInstance.add(1);
-  std::cout << std::format("\nAccumulator value: {}",
-                           accumulatorInstance.get());
-
-  accumulatorInstance.reset();
-  std::cout << std::format("\nAccumulator value: {}",
-                           accumulatorInstance.get());
+  std::cout << "Value: " << accumulatorInstance.get() << std::endl;
 
   return 0;
 }
