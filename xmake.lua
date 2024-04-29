cf1637bb8d058d20d6cf33779601fa73e4bfcfe2
Andy Zijian Zhang <futrime+github.com@outlook.com>
2024-04-29 07:39:14 +0000

    refactor: use Google Style

--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,9 @@
 add_rules("mode.debug", "mode.release")
 
 target("cpp-project-template")
-    add_files("src/**.cpp")
-    add_headerfiles("src/(**.h)")
+    add_files("src/**.cc")
     add_includedirs("src")
+    set_exceptions("cxx")
     set_kind("binary")
     set_languages("cxx20")
+    set_warnings("allextra")
